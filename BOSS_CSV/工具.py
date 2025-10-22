2735bb6f05fa9c3b4910fcaef4e88af7cf7ee0c5
sheayoo_Pad <sheayoo@qq.com>
2025-10-22 15:56:31 +0800

    2

--- BOSS_CSV/工具.py
+++ BOSS_CSV/工具.py
@@ -6,3 +6,4 @@ processor = CSVJsonProcessor("./resources")
 processor.delete_empty_rows(table_name="job_deliver",field="AI结果",empty_vals={"待分析", "暂无"})
 tool_category = ToolCategory()
 tool_category.clear_file_content("./resources/jobs.md")
+#_&&
